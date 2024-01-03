e5854a004a2c03f045b71c8ae4b0797a84260156
your_github_username <Xueyu.Wei25@ycdsbk12.ca>
2024-01-03 15:12:07 -0500

    Assignment 7

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,67 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+
+  PImage imgMeteor;
+  PImage imgMissile;
+  PImage imgBackground;
+
+  float fltMeteorX = -100;
+  float fltMeteorY = 0;
+
+  float fltMissileX = 200;
+  float fltMissileY = 400 - 50;
+
+  float fltCirX = 100;
+  float fltCirY;
+
+  float fltCirSpeedX = 2;
+  float fltCirSpeedY = 1;
+
   public void settings() {
-	// put your size call here
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    imgMeteor = loadImage("Wind.png");
+    imgMeteor.resize(imgMeteor.width / 4, imgMeteor.height / 4);
+
+    imgMissile = loadImage("rain-png-transparent-9.png");
+
+    imgBackground = loadImage("Rain Window.jpg");
+    imgBackground.resize(width, height);
+
+    fltCirY = height / 10;
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    background(128);
+    image(imgBackground, 0, 0);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // Draw Meteor and move
+    image(imgMeteor, fltMeteorX, fltMeteorY);
+    fltMeteorX += 1;
+
+    // Draw circle and move
+    ellipse(fltCirX, fltCirY, 20, 20);
+    fltCirX += fltCirSpeedX;
+    fltCirY += fltCirSpeedY;
+
+    if (fltCirX < 0 + 10 || fltCirX > width - 10) {
+      fltCirSpeedX *= -1;
+    }
+
+    if (fltCirY < 0 + 10 || fltCirY > height - 10) {
+      fltCirSpeedY *= -1;
+    }
+
+    // Draw missile and move
+    image(imgMissile, fltMissileX, fltMissileY);
+    fltMissileY -= 0.5;
   }
-  
-  // define other methods down here.
+
+  public static void main(String[] args) {
+    PApplet.main("Sketch");
+  }
 }
